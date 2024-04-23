b774ec03947bb48902b0b0a904ea5aa660b715bc
Zack Sima <simayance@gmail.com>
2024-04-22 18:19:40 -0700

    new

--- Assets/Lobby/Scripts/SelectionCard.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-public abstract class SelectionCard: MonoBehaviour {
-    public abstract string uid { get; }
-    
-    public abstract void OnSelect();
-
-    public abstract void OnDeselect();
-}
---
+++ Assets/Scripts/Lobby/LobbyUI.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Script that just deals with lobby UI. TODO: actually integrate with good visuals, etc
+/// </summary>
+
+public class LobbyUI : MonoBehaviour {
+
+	#region Statics
+
+	public static LobbyUI instance;
+
+	#endregion
+
+	#region References
+
+	//disable this if not in a lobby
+	[SerializeField] private RectTransform lobbyButtons;
+
+	//TODO: replace this with more fancy stuff (currently just a single string that displays everything)
+	[SerializeField] private TMP_Text lobbyTextDisplay;
+
+	[SerializeField] private RectTransform lobbyLoadingUI, gameStartingUI;
+
+	#endregion
+
+	#region Members
+
+	//TODO: find a better way to access lobby players than putting them in a UI script!
+	private List<LobbyPlayer> lobbyPlayers = new();
+	public void AddLobbyPlayer(LobbyPlayer p) { if (!lobbyPlayers.Contains(p)) lobbyPlayers.Add(p); }
+	public void RemoveLobbyPlayer(LobbyPlayer p) { if (lobbyPlayers.Contains(p)) lobbyPlayers.Remove(p); }
+	public List<LobbyPlayer> GetLobbyPlayers() { return lobbyPlayers; }
+
+	#endregion
+
+	#region Functions
+
+	//sends current player information (master client) to LobbyStatsSyncer script
+	public void InitLocalSync() {
+		StartCoroutine(WaitInitData());
+	}
+	private IEnumerator WaitInitData() {
+		yield return new WaitForSeconds(0.2f);
+
+		MapDropdownChanged();
+		PlayerNameInputChanged();
+	}
+	public void WaveInputChanged() {
+		if (LobbyPlayer.playerInstance == null || LobbyStatsSyncer.instance == null) return;
+
+		if (LobbyPlayer.playerInstance.Runner.IsSharedModeMasterClient) {
+			if (int.TryParse(MenuManager.instance.GetWaveInput().text, out int wave))
+				LobbyStatsSyncer.instance.SetStartingWave(wave);
+		}
+	}
+	public void MapDropdownChanged() {
+		if (LobbyPlayer.playerInstance == null || LobbyStatsSyncer.instance == null) return;
+
+		if (LobbyPlayer.playerInstance.Runner.IsSharedModeMasterClient) {
+			LobbyStatsSyncer.instance.SetMap(MenuManager.instance.GetMapDropdown().options[
+				MenuManager.instance.GetMapDropdown().value].text);
+		}
+	}
+	public void PlayerNameInputChanged() {
+		if (LobbyPlayer.playerInstance == null || LobbyStatsSyncer.instance == null) return;
+
+		if (MenuManager.instance.GetPlayerNameInput().text != "") {
+			LobbyPlayer.playerInstance.SetPlayerName(MenuManager.instance.GetPlayerNameInput().text);
+		} else {
+			LobbyPlayer.playerInstance.SetPlayerName("Player");
+		}
+	}
+	public void SetLobbyUIActive(bool isActive) {
+		if (!isActive) {
+			lobbyTextDisplay.text = "Not currently in a lobby";
+		} else {
+			lobbyTextDisplay.text = "Loading lobby...";
+		}
+		lobbyButtons.gameObject.SetActive(isActive);
+	}
+	//TODO: modify the lobby controls script to give this script actual information instead of one string
+	public void SetLobbyText(string text) {
+		lobbyTextDisplay.text = text;
+	}
+	public void QuitLobby() {
+		ServerLinker.instance.StopLobby();
+		SetLobbyUIActive(false);
+
+		Destroy(ServerLinker.instance.gameObject);
+		UnityEngine.SceneManagement.SceneManager.LoadScene(ServerLinker.LOBBY_SCENE);
+	}
+	//NOTE: this calls the singleton lobby player assuming the player exists
+	public void ToggleIsReady() {
+		if (LobbyPlayer.playerInstance == null || LobbyStatsSyncer.instance == null) return;
+
+		LobbyPlayer.playerInstance.ToggleIsReady();
+	}
+	public void SetLobbyLoading(bool loading) {
+		lobbyLoadingUI.gameObject.SetActive(loading);
+		StartCoroutine(LobbyLoadingTimeout(lobbyLoadingUI.gameObject));
+	}
+	public void SetGameStarting() {
+		gameStartingUI.gameObject.SetActive(true);
+		StartCoroutine(LobbyLoadingTimeout(gameStartingUI.gameObject));
+	}
+	//if something fishy happens with the lobby loader, don't leave the UI on forever
+	private IEnumerator LobbyLoadingTimeout(GameObject ui) {
+		for (float t = 0; t < 10f; t += Time.deltaTime) {
+			yield return null;
+			if (!ui.activeInHierarchy) {
+				Debug.Log("lobby load hidden");
+				yield break;
+			}
+		}
+		ui.SetActive(true);
+	}
+
+	private void Awake() {
+		instance = this;
+	}
+	private void Start() {
+		SetLobbyUIActive(false);
+	}
+
+	#endregion
+
+}
