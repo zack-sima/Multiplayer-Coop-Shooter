af1e72011837f565addc13009c2aa5d8f2ebff2a
Ethan Ge <pineapplespineapplesiwonderwhy@gmail.com>
2024-05-03 23:47:56 -0700

    ok

--- Assets/Scripts/Mechanics/Handlers/UpgradeHandler.cs
+++ Assets/Scripts/Mechanics/Handlers/UpgradeHandler.cs
@@ -5,33 +5,40 @@ using UnityEngine;
 using CSVParser.Init;
 using System.Runtime.CompilerServices;
 
-namespace Abilities.UpgradeHandler {
-    
-    public static partial class UpgradeHandler {
+namespace Abilities.UpgradeHandler
+{
 
-        public static void PushToUpgradeHandler(this Dictionary<string, UpgradesCatalog.UpgradeNode> dict, UpgradesCatalog.UpgradeNode n) {
+    public static partial class UpgradeHandler
+    {
+
+        public static void PushToUpgradeHandler(this Dictionary<string, UpgradesCatalog.UpgradeNode> dict, UpgradesCatalog.UpgradeNode n)
+        {
             // * networkedentity.playerinstance.getturret. < (projectile & turret changes)
             if (dict.ContainsKey(n.GetUpgradeId())) { Debug.LogWarning("Upgrade : " + n.GetUpgradeId() + " : already exists"); return; }
             dict.PushUpgrade(n);
         }
 
-        private static void PushUpgrade(this Dictionary<string, UpgradesCatalog.UpgradeNode> dict, UpgradesCatalog.UpgradeNode n) {
+        private static void PushUpgrade(this Dictionary<string, UpgradesCatalog.UpgradeNode> dict, UpgradesCatalog.UpgradeNode n)
+        {
             // * PlayerInfo.instance. ... For stat changes < 
             if (n == null || n.info == null) { Debug.LogError("Upgrade Null"); return; }
-            switch(n.upgradeName) {
-                
+            switch (n.upgradeName)
+            {
+
                 #region //?==| GENERAL.CSV |==?//
 
                 //*?=======================| Stat |=======================?*//
                 // * ANYTHING SINGLE-FRAME BASED
                 case "Braced Internals":
-                    if (n.info.TryGetModi(nameof(ModiName.MaxHP), out float biMaxHp)) {
-                        NetworkedEntity.playerInstance.GetEntity().SetMaxHealth(NetworkedEntity.playerInstance.GetEntity().GetBaseHealth() 
+                    if (n.info.TryGetModi(nameof(ModiName.MaxHP), out float biMaxHp))
+                    {
+                        NetworkedEntity.playerInstance.GetEntity().SetMaxHealth(NetworkedEntity.playerInstance.GetEntity().GetBaseHealth()
                             * biMaxHp + NetworkedEntity.playerInstance.GetEntity().GetMaxHealth());
                     }
                     break;
                 case "Dynamic Loader":
-                    if (n.info.TryGetModi(nameof(ModiName.Reload), out float dlReload) && TryGetTurret(out Turret turret)) {
+                    if (n.info.TryGetModi(nameof(ModiName.Reload), out float dlReload) && TryGetTurret(out Turret turret))
+                    {
                         turret.SetShootSpeed(dlReload * turret.GetBaseShootSpeed() + turret.GetShootSpeed());
                     }
                     break;
@@ -42,8 +49,9 @@ namespace Abilities.UpgradeHandler {
                     // Add code for Hardened Ammo here
                     break;
                 case "Hardened Armor":
-                    if (n.info.TryGetModi(nameof(ModiName.MaxHP), out float haMaxHp)) {
-                        NetworkedEntity.playerInstance.GetEntity().SetMaxHealth(NetworkedEntity.playerInstance.GetEntity().GetBaseHealth() 
+                    if (n.info.TryGetModi(nameof(ModiName.MaxHP), out float haMaxHp))
+                    {
+                        NetworkedEntity.playerInstance.GetEntity().SetMaxHealth(NetworkedEntity.playerInstance.GetEntity().GetBaseHealth()
                             * haMaxHp + NetworkedEntity.playerInstance.GetEntity().GetMaxHealth());
                     }
                     break;
@@ -66,68 +74,72 @@ namespace Abilities.UpgradeHandler {
 
                     break;
                 case "Embracing Bind":
-                
+
                     break;
                 case "Emergency Overclock":
-                
+
                     break;
                 case "Exposed Inductor":
-                
+
                     break;
                 case "Gas Canister":
-                
+
                     break;
                 case "Lighter Fluid":
-                
+
                     break;
                 case "Optical Battery":
-                
+
                     break;
                 case "R.T.G":
-                
+
                     break;
                 case "Regenerative Armor":
-                    if (n.info.TryGetModi(nameof(ModiName.Misc1), out float RAoutput)) {
-                        PlayerInfo.instance.GetAbilityList().Add((new RegenerativeArmor(RAoutput), false)); }
+                    if (n.info.TryGetModi(nameof(ModiName.Misc1), out float RAoutput))
+                    {
+                        PlayerInfo.instance.GetAbilityList().Add((new RegenerativeArmor(RAoutput), false));
+                    }
                     break;
                 case "Rocket Pods":
-                
+
                     break;
                 case "Scavengers Eye":
-                
+
                     break;
                 case "Sticky Bomb":
-                
+
                     break;
                 case "Stun Grenades":
-                
+
                     break;
                 case "Tesla Pack":
-                
+
                     break;
 
-                 //*?=======================| Projectile |=======================?*//
-                    //TODO: Projectile changes, such as burning rounds, explosive, etc.
+                //*?=======================| Projectile |=======================?*//
+                //TODO: Projectile changes, such as burning rounds, explosive, etc.
 
                 #endregion
 
                 //TODO: Place other trees here.
                 //...
 
-                default: 
+                default:
                     Debug.LogWarning("Ability " + n.GetUpgradeId() + " : does NOT exist in handler. Unable to pull Upgrade.");
                     return;
             }
             dict.Add(n.GetUpgradeId(), n);
         }
 
-        private static bool TryGetCombatEntity(out CombatEntity c) {
+        private static bool TryGetCombatEntity(out CombatEntity c)
+        {
             c = NetworkedEntity.playerInstance.GetCombatEntity();
             if (c == null) return false;
             else return true;
         }
 
-        private static bool TryGetTurret(out Turret t) {
+        private static bool TryGetTurret(out Turret t)
+        {
             TryGetCombatEntity(out CombatEntity c);
             if (c == null) { t = null; return false; }
             t = c.GetTurret();
